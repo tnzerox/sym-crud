401660f6007a765be8d17f48740543b5e5c91a3f
binish <binish.manandhar23@gmail.com>
2020-02-25 17:12:49 +0545

    Created post method with entity and sent to controller

--- src/Controller/ToDoListController.php
+++ src/Controller/ToDoListController.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use App\Entity\Task;
 
 class ToDoListController extends AbstractController
 {
@@ -15,5 +17,23 @@ class ToDoListController extends AbstractController
         return $this->render('to_do_list/index.html.twig', [
             'controller_name' => 'ToDoListController',
         ]);
+    }
+
+    /**
+     * @Route("/create", name="createtodo", methods={"POST"})
+     */
+
+    public function createTodo(Request $request)
+    {
+        
+        $title = $request->get('title');
+        $manager = $this->getDoctrine()->getManager();
+
+        $task = new Task;
+        $task->setTitle($title);
+
+        $manager->persist($task);
+        $manager->flush($task);
+        return redirectTo('/');
     }
 }
